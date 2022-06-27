7b685191bbedff072bbc50311de2053f9a0c8223
stone <home_wsl@mail.com>
2022-06-27 15:47:33 +0000

    init new project

---
+++ Source/Framework/FrameworkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FrameworkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRAMEWORK_API AFrameworkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
